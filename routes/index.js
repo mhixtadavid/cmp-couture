71a0897e50824433e9465639046af87f3c94da0f
mhixtadavid <67398368+mhixtadavid@users.noreply.github.com>
2024-07-11 14:36:20 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,7 @@ router.post("/register", function(req, res){
            return res.redirect("register");
        }
        passport.authenticate("local")(req, res, function(){
-           req.flash("success", "Welcome to CMP Couture " + user.username);
+           req.flash("success", "Welcome to CMP Couture, " + user.username);
            res.redirect("/");
        });
    });
